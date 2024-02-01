de9e7acd5722c930dc7205fcc7f4169824b44c5f
Nguyen Thanh Duy <duynt0319@gmail.com>
2024-02-01 17:27:21 +0700

    initial src

---
+++ OfficeClassRobotic/OfficeClassRobotic.DataTier/ConvertTer/DateOnlyConverterExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace OfficeClassRobotic.DataTier
+{
+    public class DateOnlyConverterExtensions: ValueConverter<DateOnly, DateTime>
+    {
+        public DateOnlyConverterExtensions()
+       : base(dateOnly => dateOnly.ToDateTime(TimeOnly.MinValue),
+           dateTime => DateOnly.FromDateTime(dateTime))
+        { }
+    }
+}
